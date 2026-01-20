bcaf006ff9f55cfa954eda63422e2b704b0091b7
Matthew Zhang <zhangmatt22@gmail.com>
2026-01-19 23:25:42 -0600

    added tab bar

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import { GOLD } from "@/src/constants/theme";
+import { GOLD, TAB_ICON_SIZE } from "@/src/constants/theme";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Tabs } from "expo-router";
 import { View } from "react-native";
@@ -8,8 +8,11 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: GOLD,
+        // tabBarActiveBackgroundColor: "#EAF2FF",
         tabBarBackground: () => (
-          <View style={{ backgroundColor: "#EAF2FF" }}></View>
+          <View
+            style={{ backgroundColor: "#EAF2FF", minHeight: "100%" }}
+          ></View>
         ),
       }}
     >
@@ -17,14 +20,36 @@ export default function TabLayout() {
         name="home/index"
         options={{
           title: "Home",
-          tabBarIcon: ({ color }) => <Ionicons name="home" />,
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="home" color={color} size={TAB_ICON_SIZE} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="workout/index"
+        options={{
+          title: "Workout",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="barbell-sharp" color={color} size={TAB_ICON_SIZE} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="community/index"
+        options={{
+          title: "Community",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="people-sharp" color={color} size={TAB_ICON_SIZE} />
+          ),
         }}
       />
       <Tabs.Screen
         name="profile/index"
         options={{
           title: "Profile",
-          tabBarIcon: ({ color }) => <Ionicons name="person" />,
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="person" color={color} size={TAB_ICON_SIZE} />
+          ),
         }}
       />
     </Tabs>
--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,32 +1,33 @@
 import { Platform, StyleSheet } from "react-native";
 
-
-export const GOLD = '#FFC107';
-export const BUTTONTEXT = '#000000';
-export const APP_BACKGROUND_COLOR = '#2F3036';
+export const GOLD = "#FFC107";
+export const BUTTONTEXT = "#000000";
+export const APP_BACKGROUND_COLOR = "#2F3036";
+export const TAB_ICON_SIZE = 28;
 
 export const FONTS = Platform.select({
   ios: {
     /** iOS `UIFontDescriptorSystemDesignDefault` */
-    sans: 'system-ui',
+    sans: "system-ui",
     /** iOS `UIFontDescriptorSystemDesignSerif` */
-    serif: 'ui-serif',
+    serif: "ui-serif",
     /** iOS `UIFontDescriptorSystemDesignRounded` */
-    rounded: 'ui-rounded',
+    rounded: "ui-rounded",
     /** iOS `UIFontDescriptorSystemDesignMonospaced` */
-    mono: 'ui-monospace',
+    mono: "ui-monospace",
   },
 
   default: {
-    sans: 'normal',
-    serif: 'serif',
-    rounded: 'normal',
-    mono: 'monospace',
+    sans: "normal",
+    serif: "serif",
+    rounded: "normal",
+    mono: "monospace",
   },
   web: {
     sans: "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif",
     serif: "Georgia, 'Times New Roman', serif",
-    rounded: "'SF Pro Rounded', 'Hiragino Maru Gothic ProN', Meiryo, 'MS PGothic', sans-serif",
+    rounded:
+      "'SF Pro Rounded', 'Hiragino Maru Gothic ProN', Meiryo, 'MS PGothic', sans-serif",
     mono: "SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
   },
 });
@@ -37,7 +38,7 @@ export const ONBOARDING = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#2F3036",
-    paddingBlock: 28
+    paddingBlock: 28,
   },
   bigText: {
     fontSize: 24,
@@ -52,48 +53,48 @@ export const ONBOARDING = StyleSheet.create({
 });
 
 export const BIG_GOLDEN_BUTTON = StyleSheet.create({
-    pressable: {
-        marginBlockStart: 24,
-        ...Platform.select({
-            "web": {
-                width: "25%"
-            },
-            "default": {
-                width: "75%"
-            }
-        }),
-    },
-    buttonView: {
-        width: "100%",
-        backgroundColor: GOLD,
-        alignItems: "center",
-        justifyContent: "center",
-        borderRadius: 10,
-        paddingBlock: 12
-    },
-    text: {
-        fontSize: 20,
-        fontWeight: 700,
-        ...FONTS
-    }
+  pressable: {
+    marginBlockStart: 24,
+    ...Platform.select({
+      web: {
+        width: "25%",
+      },
+      default: {
+        width: "75%",
+      },
+    }),
+  },
+  buttonView: {
+    width: "100%",
+    backgroundColor: GOLD,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10,
+    paddingBlock: 12,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 700,
+    ...FONTS,
+  },
 });
 
 export const TEXT_INPUT = StyleSheet.create({
-    input: {
-        color: "white",
-        borderStyle: "solid",
-        borderWidth: 2,
-        borderColor: "white",
-        borderRadius: 10,
-        marginBlock: 10,
-        ...Platform.select({
-            "web": {
-                width: "25%",
-                padding: 10
-            },
-            "default": {
-                width: "75%"
-            }
-        }),
-    }
+  input: {
+    color: "white",
+    borderStyle: "solid",
+    borderWidth: 2,
+    borderColor: "white",
+    borderRadius: 10,
+    marginBlock: 10,
+    ...Platform.select({
+      web: {
+        width: "25%",
+        padding: 10,
+      },
+      default: {
+        width: "75%",
+      },
+    }),
+  },
 });
