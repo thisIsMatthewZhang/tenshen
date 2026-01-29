b3c1a899fa42dbb30930dee40cff7b2f5a22e4e6
Matthew Zhang <zhangmatt22@gmail.com>
2026-01-29 14:52:47 -0600

    moved ExerciseCard to its own component file

--- src/components/WorkoutBuilder.tsx
+++ src/components/WorkoutBuilder.tsx
@@ -1,7 +1,6 @@
-import { LinearGradient } from "expo-linear-gradient";
+import ExerciseCard from "@/src/screens/Workout/components/ExerciseCard";
 import { useState } from "react";
 import {
-  Dimensions,
   DimensionValue,
   Modal,
   Pressable,
@@ -10,15 +9,13 @@ import {
   TextInput,
   View,
 } from "react-native";
-import { TimerPickerModal } from "react-native-timer-picker";
 import {
   APP_BACKGROUND_COLOR,
-  BLUE_DARKER,
   BLUE_LIGHTER,
-  GOLD,
   PATTERN,
   TEXT_INPUT,
 } from "../constants/theme";
+
 interface WorkoutBuilderProps {
   state: boolean;
   setState: React.Dispatch<React.SetStateAction<boolean>>;
@@ -39,97 +36,6 @@ export default function WorkoutBuilder({
   const [exerciseAdded, setExerciseAdded] = useState<boolean>(false);
   const [showTimerPicker, setShowTimerPicker] = useState<boolean>(false);
   const [timer, setTimer] = useState<"Rest Timer" | string>("Rest Timer");
-
-  const Button = ({
-    title,
-    bgColor,
-    textColor,
-    onPress,
-    width,
-  }: ButtonProps) => {
-    const buttonStyles = {
-      width: width,
-      backgroundColor: bgColor,
-      borderRadius: 10,
-      paddingHorizontal: 12,
-      paddingVertical: 8,
-      margin: 8,
-    };
-    return (
-      <Pressable
-        style={({ pressed }) => {
-          return [
-            buttonStyles,
-            {
-              opacity: pressed ? 0.5 : 1,
-            },
-          ];
-        }}
-        onPress={onPress}
-      >
-        <View style={PATTERN.center}>
-          <Text
-            style={[
-              PATTERN.smallText,
-              { color: textColor, fontWeight: "bold" },
-            ]}
-          >
-            {title}
-          </Text>
-        </View>
-      </Pressable>
-    );
-  };
-  const ExeciseCard = () => {
-    return (
-      <View style={styles.card}>
-        <View style={styles.header}>
-          <Text style={[PATTERN.bigText, { color: "black" }]}>Squats</Text>
-          <Pressable
-            style={styles.timer}
-            onPress={() => setShowTimerPicker(true)}
-          >
-            <Text
-              style={[
-                PATTERN.smallText,
-                {
-                  color: BLUE_DARKER,
-                  fontWeight: 600,
-                  textDecorationLine: "underline",
-                },
-              ]}
-            >
-              {timer}
-            </Text>
-            <TimerPickerModal
-              closeOnOverlayPress
-              LinearGradient={LinearGradient}
-              setIsVisible={setShowTimerPicker}
-              visible={showTimerPicker}
-              onConfirm={({ minutes, seconds }) => {
-                setTimer(minutes + "m " + seconds + "s");
-                setShowTimerPicker(false);
-              }}
-              onCancel={() => setShowTimerPicker(false)}
-              hideHours={true}
-              secondInterval={15}
-              styles={{ theme: "dark" }}
-              confirmButton={
-                <Button
-                  title="Confirm 👍"
-                  bgColor={BLUE_LIGHTER}
-                  textColor="white"
-                />
-              }
-              cancelButton={
-                <Button title="Cancel" bgColor="red" textColor="black" />
-              }
-            />
-          </Pressable>
-        </View>
-      </View>
-    );
-  };
 
   return (
     <Modal
@@ -161,12 +67,26 @@ export default function WorkoutBuilder({
             maxLength={40}
           />
           <View style={styles.allCards}>
-            <ExeciseCard />
+            <ExerciseCard
+              timer={timer}
+              onPress={() => setShowTimerPicker(true)}
+              pickerProps={{
+                setIsVisible: setShowTimerPicker,
+                visible: showTimerPicker,
+                onConfirm({ minutes, seconds }) {
+                  setTimer(minutes + "m " + seconds + "s");
+                  setShowTimerPicker(false);
+                },
+                onCancel() {
+                  setShowTimerPicker(false);
+                },
+              }}
+            />
           </View>
           <Button
             title="Add Exercise +"
             bgColor={BLUE_LIGHTER}
-            textColor="black"
+            textColor="white"
             onPress={() => console.error("not implemented")}
             width={"90%"}
           />
@@ -175,6 +95,44 @@ export default function WorkoutBuilder({
     </Modal>
   );
 }
+
+export const Button = ({
+  title,
+  bgColor,
+  textColor,
+  onPress,
+  width,
+}: ButtonProps) => {
+  const buttonStyles = {
+    width: width,
+    backgroundColor: bgColor,
+    borderRadius: 10,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    margin: 8,
+  };
+  return (
+    <Pressable
+      style={({ pressed }) => {
+        return [
+          buttonStyles,
+          {
+            opacity: pressed ? 0.5 : 1,
+          },
+        ];
+      }}
+      onPress={onPress}
+    >
+      <View style={PATTERN.center}>
+        <Text
+          style={[PATTERN.smallText, { color: textColor, fontWeight: "bold" }]}
+        >
+          {title}
+        </Text>
+      </View>
+    </Pressable>
+  );
+};
 
 const styles = StyleSheet.create({
   outerView: {
@@ -193,19 +151,6 @@ const styles = StyleSheet.create({
   innerView: {
     width: "100%",
   },
-  header: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  timer: {},
+
   allCards: {},
-  card: {
-    width: Dimensions.get("screen").width * 0.9,
-    height: 175,
-    backgroundColor: GOLD,
-    borderRadius: 20,
-    margin: 8,
-    padding: 12,
-  },
 });
---
+++ src/screens/Workout/components/ExerciseCard.tsx
@@ -0,0 +1,105 @@
+import { Button } from "@/src/components/WorkoutBuilder";
+import {
+    BLUE_DARKER,
+    BLUE_LIGHTER,
+    GOLD,
+    PATTERN,
+} from "@/src/constants/theme";
+import { LinearGradient } from "expo-linear-gradient";
+import { ComponentPropsWithoutRef } from "react";
+import { Dimensions, Pressable, StyleSheet, Text, View } from "react-native";
+import { TimerPickerModal } from "react-native-timer-picker";
+
+interface ExerciseCardProps {
+  timer: string;
+  onPress: () => void;
+  pickerProps: ComponentPropsWithoutRef<typeof TimerPickerModal>;
+}
+const setSegment = () => {};
+
+export default function ExerciseCard({
+  timer,
+  onPress,
+  pickerProps,
+}: ExerciseCardProps) {
+  return (
+    <View style={styles.card}>
+      <View style={styles.title}>
+        <Text style={[PATTERN.bigText, { color: "black" }]}>Squats</Text>
+        <Pressable onPress={onPress}>
+          <Text
+            style={[
+              PATTERN.smallText,
+              {
+                color: BLUE_DARKER,
+                fontWeight: 800,
+                textDecorationLine: "underline",
+              },
+            ]}
+          >
+            {timer}
+          </Text>
+          <TimerPickerModal
+            closeOnOverlayPress
+            LinearGradient={LinearGradient}
+            setIsVisible={pickerProps.setIsVisible}
+            visible={pickerProps.visible}
+            onConfirm={pickerProps.onConfirm}
+            onCancel={pickerProps.onCancel}
+            hideHours={true}
+            secondInterval={15}
+            styles={{ theme: "dark" }}
+            confirmButton={
+              <Button
+                title="Confirm 👍"
+                bgColor={BLUE_LIGHTER}
+                textColor="white"
+              />
+            }
+            cancelButton={
+              <Button title="Cancel" bgColor="red" textColor="black" />
+            }
+          />
+        </Pressable>
+      </View>
+      <View style={styles.cardBottom}>
+        <View style={styles.headers}>
+          <Text>Sets</Text>
+          <Text>Lbs</Text>
+          <Text>Reps</Text>
+        </View>
+        <View
+          style={[
+            PATTERN.separator,
+            { backgroundColor: "black", marginVertical: 0 },
+          ]}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    width: Dimensions.get("screen").width * 0.9,
+    height: 200,
+    backgroundColor: GOLD,
+    borderRadius: 20,
+    margin: 8,
+  },
+  title: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginHorizontal: 16,
+    marginVertical: 8,
+  },
+
+  headers: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginHorizontal: 16,
+  },
+  cardBottom: { width: "100%" },
+});
