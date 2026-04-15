b47bd2f62ab15bb771fb277b0ac1aa3810aedfd5
Matthew Zhang <zhangmatt22@gmail.com>
2026-04-14 20:53:23 -0500

    moved workouts and exercise context to TabLayout

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,51 +1,80 @@
 import { firebaseConfigWeb } from "@/config/firebaseConfig";
 import { ICON_SIZE, MAIN_COLOR } from "@/src/constants/theme";
+import { ExerciseContext } from "@/src/contexts/ExerciseContext";
+import { WorkoutsContext } from "@/src/contexts/WorkoutsContext";
+import { ExerciseCard } from "@/src/types/exercisecard";
+import { User as AppUser } from "@/src/types/user";
+import { Workout } from "@/src/types/workout";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Tabs } from "expo-router";
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { useState } from "react";
 import { View } from "react-native";
+import { getUserData } from "./profile";
 
 const app = initializeApp(firebaseConfigWeb);
 const auth = getAuth(app);
+const db = getFirestore(app);
+let appUserData: {
+  [K in keyof AppUser]?: AppUser[K];
+} = {};
+
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    const userData = await getUserData(user.uid, db);
+    appUserData = { ...userData };
+  }
+});
 
 export default function TabLayout() {
+  const [exercises, setExercises] = useState<ExerciseCard[]>([]);
+  const [workouts, setWorkouts] = useState<Workout[]>(
+    appUserData.workoutsSaved!,
+  );
   const user = auth.currentUser!;
   const isVerified = user.emailVerified;
   return (
-    <Tabs
-      detachInactiveScreens={false}
-      screenOptions={{
-        tabBarActiveTintColor: MAIN_COLOR,
-        // tabBarActiveBackgroundColor: "#EAF2FF",
-        tabBarBackground: () => (
-          <View style={{ backgroundColor: "#494A50", minHeight: "100%" }} />
-        ),
-      }}
-    >
-      <Tabs.Protected guard={isVerified}>
-        <Tabs.Screen
-          name="home/index"
-          options={{
-            title: "Home",
-            animation: "shift",
-            tabBarIcon: ({ color }) => (
-              <Ionicons name="home" color={color} size={ICON_SIZE} />
+    <WorkoutsContext.Provider value={[workouts, setWorkouts]}>
+      <ExerciseContext.Provider value={[exercises, setExercises]}>
+        <Tabs
+          detachInactiveScreens={false}
+          screenOptions={{
+            tabBarActiveTintColor: MAIN_COLOR,
+            // tabBarActiveBackgroundColor: "#EAF2FF",
+            tabBarBackground: () => (
+              <View style={{ backgroundColor: "#494A50", minHeight: "100%" }} />
             ),
           }}
-        />
-        <Tabs.Screen
-          name="workout/index"
-          options={{
-            animation: "shift",
+        >
+          <Tabs.Protected guard={isVerified}>
+            <Tabs.Screen
+              name="home/index"
+              options={{
+                title: "Home",
+                animation: "shift",
+                tabBarIcon: ({ color }) => (
+                  <Ionicons name="home" color={color} size={ICON_SIZE} />
+                ),
+              }}
+            />
+            <Tabs.Screen
+              name="workout/index"
+              options={{
+                animation: "shift",
 
-            title: "Workout",
-            tabBarIcon: ({ color }) => (
-              <Ionicons name="barbell-sharp" color={color} size={ICON_SIZE} />
-            ),
-          }}
-        />
-        {/* <Tabs.Screen
+                title: "Workout",
+                tabBarIcon: ({ color }) => (
+                  <Ionicons
+                    name="barbell-sharp"
+                    color={color}
+                    size={ICON_SIZE}
+                  />
+                ),
+              }}
+            />
+            {/* <Tabs.Screen
         name="community/index"
         options={{
           title: "Community",
@@ -54,18 +83,20 @@ export default function TabLayout() {
           ),
         }}
       /> */}
-        <Tabs.Screen
-          name="profile/index"
-          options={{
-            animation: "shift",
+            <Tabs.Screen
+              name="profile/index"
+              options={{
+                animation: "shift",
 
-            title: "Profile",
-            tabBarIcon: ({ color }) => (
-              <Ionicons name="person" color={color} size={ICON_SIZE} />
-            ),
-          }}
-        />
-      </Tabs.Protected>
-    </Tabs>
+                title: "Profile",
+                tabBarIcon: ({ color }) => (
+                  <Ionicons name="person" color={color} size={ICON_SIZE} />
+                ),
+              }}
+            />
+          </Tabs.Protected>
+        </Tabs>
+      </ExerciseContext.Provider>
+    </WorkoutsContext.Provider>
   );
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,5 @@
-import { ExerciseCard } from "@/src/types/exercisecard";
 import { Slot } from "expo-router";
-import { useState } from "react";
-import { ExerciseContext } from "../contexts/ExerciseContext";
-import { WorkoutsContext } from "../contexts/WorkoutsContext";
-import { Workout } from "../types/workout";
 
 export default function RootLayout() {
-  const [exercises, setExercises] = useState<ExerciseCard[]>([]);
-  const [workout, setWorkout] = useState<Workout[]>([]);
-  return (
-    <WorkoutsContext.Provider value={[workout, setWorkout]}>
-      <ExerciseContext.Provider value={[exercises, setExercises]}>
-        <Slot />
-      </ExerciseContext.Provider>
-    </WorkoutsContext.Provider>
-  );
+  return <Slot />;
 }
